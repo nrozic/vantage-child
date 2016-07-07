c0439a7fe09b6bfb19cea40b3ecba8c31ceac4e9
Nikola Rožić <nikola@trilium.io>
2016-07-07 13:02:59 +0200

    Added missing echo to echo out FAB link in header.php :)

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ include( get_template_directory() . '/header.php' );
 if( ! is_home() && ! is_front_page() && ! empty( $GLOBALS['redux_vantage']['fab-url'] ) ) : ?>
     
     <div class="vantage_child_fab">
-        <a class="vantage_child_link" href="<?php $GLOBALS['redux_vantage']['fab-url'] ?>" title="Contact Us">
+        <a class="vantage_child_link" href="<?php echo $GLOBALS['redux_vantage']['fab-url'] ?>" title="Contact Us">
             <i class="fa fa-envelope" aria-hidden="true"></i>
         </a>
     </div>
